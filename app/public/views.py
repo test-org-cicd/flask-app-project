ebb13ed5d1ef3e102c1b41332f796e81e1005849
Himadri Ganguly <himadri.tech@gmail.com>
2022-03-03 20:39:28 +0530

    Add version to the home page

--- app/public/views.py
+++ app/public/views.py
@@ -8,6 +8,10 @@ blueprint = Blueprint("public", __name__, static_folder="../static")
 def homepage():
     user_agent = request.headers.get('User-Agent')
     hostname = socket.gethostname()
+    f = open('./VERSION')
+    version = f.read().splitlines()[0]
+    f.close()
     return render_template("public/home.html",
                            user_agent=user_agent,
-                           hostname=hostname)
+                           hostname=hostname,
+                           version=version)
